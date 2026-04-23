165d8c1deb0c78e3d4dd3aae86b9868c6b4c519b
Perth <zxu197310@gmail.com>
2026-04-23 20:40:33 +0800

    轮播图搜索框和导航栏

--- lib/components--存放公共组件/home/Slider.dart
+++ lib/components--存放公共组件/home/Slider.dart
@@ -11,12 +11,16 @@ class HmSilder extends StatefulWidget {
 }
 
 class _HmSilderState extends State<HmSilder> {
-  
-
-  Widget _getSlider()
+  int _currentIndex=0;
+  final CarouselSliderController _controller=CarouselSliderController();
+Widget _getSlider()
   {
     final double screenWidth =MediaQuery.of(context).size.width;
-    return CarouselSlider(items:List.generate(widget.bannerList.length, (index){
+    return CarouselSlider(
+      carouselController: _controller,
+      items:List.generate(
+      widget.bannerList.length, 
+    (index){
       return Image.network(widget.bannerList[index].imgUrl,
       fit:BoxFit.cover,
       width:screenWidth,);
@@ -24,13 +28,72 @@ class _HmSilderState extends State<HmSilder> {
       viewportFraction: 1,
       autoPlay: true,
       height: 280,
-      autoPlayInterval: Duration(seconds: 5)
+      autoPlayInterval: Duration(seconds: 5),
+      onPageChanged: (index, reason) {
+        _currentIndex=index;
+        setState(() {});
+      },
     ));
   }
+
+    Widget _getSearch(){
+    return Positioned(
+      top: 10,
+      left: 0,
+      right: 0,
+      child: Padding(padding: EdgeInsetsGeometry.all(10),
+      child: Container(
+        alignment: Alignment.centerLeft,
+        padding: EdgeInsets.symmetric(horizontal: 40),
+        height: 50, 
+        decoration: BoxDecoration(
+          color: const Color.fromRGBO(0, 0, 0, 0.4),
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Text(
+          "搜索...",
+          style: TextStyle(color: Colors.white,fontSize: 16),
+        ),
+      ),),
+          );
+  }
+
+  Widget _getDot(){
+    return Positioned(
+      right: 0,
+      left: 0,
+      bottom: 10,
+      child:SizedBox(
+        height: 40,
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(widget.bannerList.length, (index){
+            return GestureDetector(
+              onTap: () {
+                _controller.animateToPage(index);
+                _currentIndex=index;
+                setState(() {});
+              },
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+              margin: EdgeInsets.only(left: 10),
+              height: 6,
+              width:index==_currentIndex? 40:20,
+              decoration: BoxDecoration(color:index==_currentIndex? Colors.white:Color.fromRGBO(0, 0, 0, 0.3),borderRadius: BorderRadius.circular(3)),
+            )
+            );
+          }),
+        ),
+      )
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Stack(
-      children: [_getSlider()],
+      children: [_getSlider(),_getSearch(),_getDot()],
     );
 
     // return Padding(padding:EdgeInsetsGeometry.symmetric(horizontal: 0),
