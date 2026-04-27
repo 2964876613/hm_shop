d3d924f1df000d8906f913969ba49e2b3f369500
Perth <zxu197310@gmail.com>
2026-04-27 11:22:35 +0800

    登录页面搭建和表单校验

--- lib/utils--存放工具类/toastUtls.dart
+++ lib/utils--存放工具类/toastUtls.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 
 class ToastUtls {
+  //阀门控制
+  static bool showLoading=false;
   static void showToast(BuildContext context,String? msg){
+    if(ToastUtls.showLoading)
+    {
+      return;
+    }
+    ToastUtls.showLoading=true;
+    Future.delayed(Duration(seconds: 3), () {
+      ToastUtls.showLoading = false;
+    });
     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-    width: 120,
+    width: 150,
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadiusGeometry.circular(40)
     ),
